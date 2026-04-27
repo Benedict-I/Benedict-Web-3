aa8a7d79638b94a2c1bcca956d33baf25093bf9d
Benedict-I <benedictisfiv@gmail.com>
2026-04-27 21:08:08 +0000

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,26 @@
+const firebaseConfig = {
+  apiKey: "...",
+  authDomain: "benedict-contacts.firebaseapp.com",
+  projectId: "benedict-contacts",
+  storageBucket: "benedict-contacts.firebasestorage.app",
+  messagingSenderId: "184194579113",
+  appId: "1:184194579113:web:410e070b113a96fa27c7b8"
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "benedict-contacts",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "..."
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
