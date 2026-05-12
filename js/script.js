4b2a78c1bce4f0afbf97492d8b037afbbfce779c
Benedict-I <benedictisfiv@gmail.com>
2026-05-12 00:15:56 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -587,3 +587,83 @@ if(torusCanvas){
 
 
 
+/* =========================
+contact
+========================= */
+const galaxyCanvas = document.getElementById("galaxy-canvas");
+
+if (galaxyCanvas) {
+
+    const scene = new THREE.Scene();
+
+    const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    );
+
+    const renderer = new THREE.WebGLRenderer({
+        canvas: galaxyCanvas,
+        alpha: true
+    });
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    const particlesGeometry = new THREE.BufferGeometry();
+
+    const particlesCount = 5000;
+
+    const posArray = new Float32Array(particlesCount * 3);
+
+    for(let i = 0; i < particlesCount * 3; i++){
+
+        posArray[i] = (Math.random() - 0.5) * 25;
+    }
+
+    particlesGeometry.setAttribute(
+        'position',
+        new THREE.BufferAttribute(posArray, 3)
+    );
+
+    const particlesMaterial = new THREE.PointsMaterial({
+        size: 0.02,
+        color: 0xff8800
+    });
+
+    const particlesMesh = new THREE.Points(
+        particlesGeometry,
+        particlesMaterial
+    );
+
+    scene.add(particlesMesh);
+
+    camera.position.z = 5;
+
+    function animate(){
+
+        requestAnimationFrame(animate);
+
+        particlesMesh.rotation.y += 0.0008;
+
+        particlesMesh.rotation.x += 0.0003;
+
+        renderer.render(scene, camera);
+    }
+
+    animate();
+
+    window.addEventListener('resize', () => {
+
+        renderer.setSize(window.innerWidth, window.innerHeight);
+
+        camera.aspect = window.innerWidth / window.innerHeight;
+
+        camera.updateProjectionMatrix();
+    });
+}
+
+
+/* =========================
+portfolio
+========================= */
