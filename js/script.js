c8633d5e5eb795ac9b5a3b0a90eec13c557ffef9
Benedict-I <benedictisfiv@gmail.com>
2026-05-12 00:09:07 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -513,3 +513,77 @@ window.addEventListener("resize", () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const torusCanvas = document.getElementById("torus-canvas");
+
+if(torusCanvas){
+
+    const scene = new THREE.Scene();
+
+    const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth/window.innerHeight,
+        0.1,
+        1000
+    );
+
+    const renderer = new THREE.WebGLRenderer({
+        canvas: torusCanvas,
+        alpha:true
+    });
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    const geometry = new THREE.TorusKnotGeometry(
+        1,
+        0.3,
+        300,
+        32
+    );
+
+    const material = new THREE.MeshBasicMaterial({
+        color:0x2196f3,
+        wireframe:true
+    });
+
+    const torus = new THREE.Mesh(
+        geometry,
+        material
+    );
+
+    scene.add(torus);
+
+    camera.position.z = 4;
+
+    function animate(){
+
+        requestAnimationFrame(animate);
+
+        torus.rotation.x += 0.003;
+
+        torus.rotation.y += 0.004;
+
+        renderer.render(scene,camera);
+    }
+
+    animate();
+}
+
+
+
+
+
+
